9be4cfce8ea76a1a699e94344621c2dcc390c1f7
yexu <xye012286@163.com>
2019-05-16 00:20:39 +0800

    commit 2019/5/16 0:20

--- src/components/home/BatchQuery.vue
+++ src/components/home/BatchQuery.vue
@@ -1,13 +1,82 @@
 <template>
-  <h1>批量查询</h1>
+  <div class="batchQuery">
+    <!--<h3>批量查询</h3>-->
+    <div class="search">
+      <div>
+        <h3>输入要查找的城市</h3>
+        <input type="text" placeholder="城市" v-model="city" style="padding:4px;"/>
+        <button @click="search_city" style="padding:4px;">查询</button>
+      </div>
+    </div>
+  </div>
 </template>
 
 <script>
-    export default {
-        name: ""
+  export default {
+    name: "",
+    data() {
+      return {
+        city:''
+      }
+    },
+    methods:{
+      // search2(){
+      //   this.$axios({
+      //     url:''
+      //   }).then(res=>{
+      //
+      //   })
+      // },
+      search_city(){
+        console.log(this.city)
+        let params = new URLSearchParams();
+        params.append("city", this.city);
+        this.$axios({
+          url:'getAllUserCorrespondDeviceBycity.do',
+          method:'post',
+          data:params
+        }).then(res=>{
+          // console.log(res.data.data)
+          let params2 = new URLSearchParams();
+          params2.append("username", res.data.data);
+          setInterval(function(){
+
+            this.$axios({
+              url:'getAllUserCorrespondDeviceBycity.do',
+              method:'post',
+              data:params
+            }).then(res=>{
+
+            })
+          },10)
+        })
+      }
+    },
+    mounted(){
     }
+  }
 </script>
 
 <style scoped>
+  * {
+    -webkit-box-sizing: border-box;
+    -moz-box-sizing: border-box;
+    box-sizing: border-box;
 
+  }
+
+  .batchQuery {
+    width: 1200px;
+    margin: 0 auto;
+    text-align: left;
+  }
+
+  .search {
+    display: flex;
+    flex-flow: row nowrap;
+  }
+
+  .search > div {
+    flex: 1;
+  }
 </style>
